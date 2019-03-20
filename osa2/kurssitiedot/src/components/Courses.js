c5bc0cd410c4953160442effd5efb62cb321a509
Timo Hopia <timo.hopia@innofactor.com>
2019-03-20 23:55:32 +0200

    kurssitiedot sovellus valmis

---
+++ osa2/kurssitiedot/src/components/Courses.js
@@ -0,0 +1,72 @@
+import React from 'react'
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>
+                {props.course}
+            </h1>
+        </div>
+    )
+  }
+
+const Part = ({part}) => {
+    return (
+        <div>
+            <p>
+                {part.name} {part.exercises}
+            </p>
+        </div>
+    )
+}
+const Content = ({parts}) => {
+    const rows = () => parts.map(part =>
+        <Part 
+        key={part.id}
+        part={part}
+        />
+      )
+    return (
+        <div>
+            {rows()}
+        </div>
+    )
+}
+
+const Total = ({parts}) => {
+    const reducer = (accumulator, currentValue) => {
+        return accumulator + currentValue.exercises
+    }
+    const total = parts.reduce(reducer,0)
+    return (
+        <div>
+            yhteensä {total} tehtävää
+        </div>
+    )
+}
+
+const Course = (props) => {
+    return (
+        <div>
+            <Header course={props.course.name} />
+            <Content parts={props.course.parts} />
+            <Total parts={props.course.parts} />
+        </div>
+    )
+}
+
+const Courses = ({courses}) => {
+    const rows = () => courses.map(course =>
+        <Course 
+        key={course.id}
+        course={course}
+        />
+      )
+    return (
+        <div>
+            {rows()}
+        </div>
+    )
+}
+
+export default Courses
